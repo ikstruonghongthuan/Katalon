8174982f9671e0ca9c8c4823fbd2b0ac63db91a4
ikstruonghongthuan <ikstruonghongthuan@gmail.com>
2024-05-31 23:56:30 +0700

    hello

---
+++ Object Repository/Thuan/Page_Creative tools for music makers  Ableton/a_Download the Live Set of Nadia Struiwighs_6037fd.rs
@@ -0,0 +1,138 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<WebElementEntity>
+   <description></description>
+   <name>a_Download the Live Set of Nadia Struiwighs_6037fd</name>
+   <tag></tag>
+   <elementGuidId>2bbd07f7-a0f3-488e-87ce-d2d7ee75af69</elementGuidId>
+   <selectorCollection>
+      <entry>
+         <key>CSS</key>
+         <value>div.blog-teaser.blog-teaser--category-downloads > div.blog-teaser__text-wrap > h2.blog-teaser__headline > a</value>
+      </entry>
+      <entry>
+         <key>XPATH</key>
+         <value>//div[@id='main']/div/main/section/div/div[3]/div/div/h2/a</value>
+      </entry>
+   </selectorCollection>
+   <selectorMethod>XPATH</selectorMethod>
+   <smartLocatorCollection>
+      <entry>
+         <key>SMART_LOCATOR</key>
+         <value>internal:role=link[name=&quot;Download the Live Set of Nadia Struiwigh’s New Track “Leelow Waters”&quot;i]</value>
+      </entry>
+   </smartLocatorCollection>
+   <smartLocatorEnabled>false</smartLocatorEnabled>
+   <useRalativeImagePath>true</useRalativeImagePath>
+   <webElementProperties>
+      <isSelected>false</isSelected>
+      <matchCondition>equals</matchCondition>
+      <name>tag</name>
+      <type>Main</type>
+      <value>a</value>
+      <webElementGuid>ed3fa49b-ec2f-48f8-a089-0186f9f111ca</webElementGuid>
+   </webElementProperties>
+   <webElementProperties>
+      <isSelected>true</isSelected>
+      <matchCondition>equals</matchCondition>
+      <name>href</name>
+      <type>Main</type>
+      <value>/en/blog/download-the-live-set-of-nadia-struiwighs-new-track-leelow-waters/</value>
+      <webElementGuid>979d73e6-1104-4b9e-9859-81d3ce237453</webElementGuid>
+   </webElementProperties>
+   <webElementProperties>
+      <isSelected>true</isSelected>
+      <matchCondition>equals</matchCondition>
+      <name>text</name>
+      <type>Main</type>
+      <value>Download the Live Set of Nadia Struiwigh’s New Track “Leelow Waters”</value>
+      <webElementGuid>3ac333ac-08e5-4b30-9f99-76b96a94c467</webElementGuid>
+   </webElementProperties>
+   <webElementProperties>
+      <isSelected>false</isSelected>
+      <matchCondition>equals</matchCondition>
+      <name>xpath</name>
+      <type>Main</type>
+      <value>id(&quot;main&quot;)/div[@class=&quot;page&quot;]/main[1]/section[@class=&quot;panel-blog-teasers js-blog&quot;]/div[@class=&quot;blog-teaser-grid&quot;]/div[@class=&quot;cell&quot;]/div[@class=&quot;blog-teaser blog-teaser--category-downloads&quot;]/div[@class=&quot;blog-teaser__text-wrap&quot;]/h2[@class=&quot;blog-teaser__headline&quot;]/a[1]</value>
+      <webElementGuid>308a5ee3-db62-499c-a0f5-d13173b82581</webElementGuid>
+   </webElementProperties>
+   <webElementXpaths>
+      <isSelected>true</isSelected>
+      <matchCondition>equals</matchCondition>
+      <name>xpath:idRelative</name>
+      <type>Main</type>
+      <value>//div[@id='main']/div/main/section/div/div[3]/div/div/h2/a</value>
+      <webElementGuid>14504fe6-eda5-4d0b-9ae5-dba74c13022d</webElementGuid>
+   </webElementXpaths>
+   <webElementXpaths>
+      <isSelected>false</isSelected>
+      <matchCondition>equals</matchCondition>
+      <name>xpath:link</name>
+      <type>Main</type>
+      <value>//a[contains(text(),'Download the Live Set of Nadia Struiwigh’s New Track “Leelow Waters”')]</value>
+      <webElementGuid>3b370157-94c3-47a1-904b-889f41add3f0</webElementGuid>
+   </webElementXpaths>
+   <webElementXpaths>
+      <isSelected>false</isSelected>
+      <matchCondition>equals</matchCondition>
+      <name>xpath:neighbor</name>
+      <type>Main</type>
+      <value>(.//*[normalize-space(text()) and normalize-space(.)='Downloads'])[1]/following::a[1]</value>
+      <webElementGuid>310a9f20-a1d6-45f2-936f-7949d972e1ad</webElementGuid>
+   </webElementXpaths>
+   <webElementXpaths>
+      <isSelected>false</isSelected>
+      <matchCondition>equals</matchCondition>
+      <name>xpath:neighbor</name>
+      <type>Main</type>
+      <value>(.//*[normalize-space(text()) and normalize-space(.)='View posts in category:'])[3]/following::a[1]</value>
+      <webElementGuid>52c64661-e6b0-4c04-b482-6fd0c13586b8</webElementGuid>
+   </webElementXpaths>
+   <webElementXpaths>
+      <isSelected>false</isSelected>
+      <matchCondition>equals</matchCondition>
+      <name>xpath:neighbor</name>
+      <type>Main</type>
+      <value>(.//*[normalize-space(text()) and normalize-space(.)='All posts'])[1]/preceding::a[1]</value>
+      <webElementGuid>56d8f05f-48e8-41d4-b192-16002ff4333e</webElementGuid>
+   </webElementXpaths>
+   <webElementXpaths>
+      <isSelected>false</isSelected>
+      <matchCondition>equals</matchCondition>
+      <name>xpath:neighbor</name>
+      <type>Main</type>
+      <value>(.//*[normalize-space(text()) and normalize-space(.)='Artists'])[3]/preceding::a[2]</value>
+      <webElementGuid>c1d627c6-f2d0-4c90-b4d0-79a223646914</webElementGuid>
+   </webElementXpaths>
+   <webElementXpaths>
+      <isSelected>false</isSelected>
+      <matchCondition>equals</matchCondition>
+      <name>xpath:neighbor</name>
+      <type>Main</type>
+      <value>//*/text()[normalize-space(.)='Download the Live Set of Nadia Struiwigh’s New Track “Leelow Waters”']/parent::*</value>
+      <webElementGuid>00d5224e-14ad-4fd6-b9e7-df90c5b8d934</webElementGuid>
+   </webElementXpaths>
+   <webElementXpaths>
+      <isSelected>false</isSelected>
+      <matchCondition>equals</matchCondition>
+      <name>xpath:href</name>
+      <type>Main</type>
+      <value>(//a[contains(@href, '/en/blog/download-the-live-set-of-nadia-struiwighs-new-track-leelow-waters/')])[2]</value>
+      <webElementGuid>87279182-92a1-4f47-aec5-227d220cc1ed</webElementGuid>
+   </webElementXpaths>
+   <webElementXpaths>
+      <isSelected>false</isSelected>
+      <matchCondition>equals</matchCondition>
+      <name>xpath:position</name>
+      <type>Main</type>
+      <value>//div[3]/div/div/h2/a</value>
+      <webElementGuid>009e39c3-4d8d-462f-9b54-c7ca58b3dec6</webElementGuid>
+   </webElementXpaths>
+   <webElementXpaths>
+      <isSelected>false</isSelected>
+      <matchCondition>equals</matchCondition>
+      <name>xpath:customAttributes</name>
+      <type>Main</type>
+      <value>//a[@href = '/en/blog/download-the-live-set-of-nadia-struiwighs-new-track-leelow-waters/' and (text() = 'Download the Live Set of Nadia Struiwigh’s New Track “Leelow Waters”' or . = 'Download the Live Set of Nadia Struiwigh’s New Track “Leelow Waters”')]</value>
+      <webElementGuid>ac769bf8-c18a-4120-a1fd-3a58620939d6</webElementGuid>
+   </webElementXpaths>
+</WebElementEntity>
---
+++ Object Repository/Thuan/Page_Creative tools for music makers  Ableton/h2_Download the Live Set of Nadia Struiwigh_74f87c.rs
@@ -0,0 +1,106 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<WebElementEntity>
+   <description></description>
+   <name>h2_Download the Live Set of Nadia Struiwigh_74f87c</name>
+   <tag></tag>
+   <elementGuidId>7f69675d-5de7-40ee-b68d-8fcaa4e6718a</elementGuidId>
+   <selectorCollection>
+      <entry>
+         <key>CSS</key>
+         <value>div.blog-teaser.blog-teaser--category-downloads > div.blog-teaser__text-wrap > h2.blog-teaser__headline</value>
+      </entry>
+      <entry>
+         <key>XPATH</key>
+         <value>//div[@id='main']/div/main/section/div/div[3]/div/div/h2</value>
+      </entry>
+   </selectorCollection>
+   <selectorMethod>XPATH</selectorMethod>
+   <smartLocatorCollection>
+      <entry>
+         <key>SMART_LOCATOR</key>
+         <value>internal:role=heading[name=&quot;Download the Live Set of Nadia Struiwigh’s New Track “Leelow Waters”&quot;i]</value>
+      </entry>
+   </smartLocatorCollection>
+   <smartLocatorEnabled>false</smartLocatorEnabled>
+   <useRalativeImagePath>true</useRalativeImagePath>
+   <webElementProperties>
+      <isSelected>false</isSelected>
+      <matchCondition>equals</matchCondition>
+      <name>tag</name>
+      <type>Main</type>
+      <value>h2</value>
+      <webElementGuid>cb99c43b-3349-4dd1-af24-d1d573406516</webElementGuid>
+   </webElementProperties>
+   <webElementProperties>
+      <isSelected>false</isSelected>
+      <matchCondition>equals</matchCondition>
+      <name>class</name>
+      <type>Main</type>
+      <value>blog-teaser__headline</value>
+      <webElementGuid>13705b43-69d1-4e28-97f2-789026bd019d</webElementGuid>
+   </webElementProperties>
+   <webElementProperties>
+      <isSelected>true</isSelected>
+      <matchCondition>equals</matchCondition>
+      <name>text</name>
+      <type>Main</type>
+      <value>Download the Live Set of Nadia Struiwigh’s New Track “Leelow Waters”</value>
+      <webElementGuid>3bff3f69-65cf-4bbf-98e4-1b4944db10ee</webElementGuid>
+   </webElementProperties>
+   <webElementProperties>
+      <isSelected>false</isSelected>
+      <matchCondition>equals</matchCondition>
+      <name>xpath</name>
+      <type>Main</type>
+      <value>id(&quot;main&quot;)/div[@class=&quot;page&quot;]/main[1]/section[@class=&quot;panel-blog-teasers js-blog&quot;]/div[@class=&quot;blog-teaser-grid&quot;]/div[@class=&quot;cell&quot;]/div[@class=&quot;blog-teaser blog-teaser--category-downloads&quot;]/div[@class=&quot;blog-teaser__text-wrap&quot;]/h2[@class=&quot;blog-teaser__headline&quot;]</value>
+      <webElementGuid>8e6d664a-b9af-404c-95fa-a0dd3fa5bb89</webElementGuid>
+   </webElementProperties>
+   <webElementXpaths>
+      <isSelected>true</isSelected>
+      <matchCondition>equals</matchCondition>
+      <name>xpath:idRelative</name>
+      <type>Main</type>
+      <value>//div[@id='main']/div/main/section/div/div[3]/div/div/h2</value>
+      <webElementGuid>9654c16c-611d-46ef-a180-deeb2f8e2e7e</webElementGuid>
+   </webElementXpaths>
+   <webElementXpaths>
+      <isSelected>false</isSelected>
+      <matchCondition>equals</matchCondition>
+      <name>xpath:neighbor</name>
+      <type>Main</type>
+      <value>(.//*[normalize-space(text()) and normalize-space(.)='Downloads'])[1]/following::h2[1]</value>
+      <webElementGuid>b34b7d61-bad2-4adf-a786-8f145dfd7fb4</webElementGuid>
+   </webElementXpaths>
+   <webElementXpaths>
+      <isSelected>false</isSelected>
+      <matchCondition>equals</matchCondition>
+      <name>xpath:neighbor</name>
+      <type>Main</type>
+      <value>(.//*[normalize-space(text()) and normalize-space(.)='View posts in category:'])[3]/following::h2[1]</value>
+      <webElementGuid>341bcea4-27d6-47b2-b1e9-9319e8ad7a8c</webElementGuid>
+   </webElementXpaths>
+   <webElementXpaths>
+      <isSelected>false</isSelected>
+      <matchCondition>equals</matchCondition>
+      <name>xpath:neighbor</name>
+      <type>Main</type>
+      <value>(.//*[normalize-space(text()) and normalize-space(.)='All posts'])[1]/preceding::h2[1]</value>
+      <webElementGuid>fd86a971-5aa5-44a7-96d1-9e84b53596e5</webElementGuid>
+   </webElementXpaths>
+   <webElementXpaths>
+      <isSelected>false</isSelected>
+      <matchCondition>equals</matchCondition>
+      <name>xpath:position</name>
+      <type>Main</type>
+      <value>//div[3]/div/div/h2</value>
+      <webElementGuid>18ef3a39-2d60-4943-b825-e0530a5e466c</webElementGuid>
+   </webElementXpaths>
+   <webElementXpaths>
+      <isSelected>false</isSelected>
+      <matchCondition>equals</matchCondition>
+      <name>xpath:customAttributes</name>
+      <type>Main</type>
+      <value>//h2[(text() = 'Download the Live Set of Nadia Struiwigh’s New Track “Leelow Waters”' or . = 'Download the Live Set of Nadia Struiwigh’s New Track “Leelow Waters”')]</value>
+      <webElementGuid>6e2c7f06-673b-4206-839a-9e1c3f3b07dd</webElementGuid>
+   </webElementXpaths>
+</WebElementEntity>
